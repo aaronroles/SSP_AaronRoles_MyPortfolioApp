b510f10d20fed9d97966caed8d8116235e7f379b
Aaron Roles <aaron_roles93@hotmail.com>
2016-04-26 14:20:05 +0100

    Fixing up the look

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+  console.log("Got home page");
 });
 
 module.exports = router;
